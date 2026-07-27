fe649b0c2188f10764181b8971c7e9a5b35a7d22
Cristianromero1099 <cristianrom1007@gmail.com>
2026-07-27 09:56:41 -0600

    Create app.py

---
+++ app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Football Performance Calculator",
+    page_icon="⚽",
+    layout="wide"
+)
+
+st.title("⚽ Football Performance Calculator")
+
+st.write("Bienvenido a mi primera aplicación de análisis táctico.")
